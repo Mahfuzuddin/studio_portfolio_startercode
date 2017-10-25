1e9ed038e598941d184aa2e5e6ecefae7a4de8a8
mahfuzuddin <mahfuzuddin347@gmail.com>
2017-10-25 22:21:08 +0000

    lots of progress

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+ var config = {
+    apiKey: "AIzaSyAqvdI7n-ZIUCfzszekIsw-r0WnkiLiagM",
+    authDomain: "my-portfolio-6092.firebaseapp.com",
+    databaseURL: "https://my-portfolio-6092.firebaseio.com",
+    projectId: "my-portfolio-6092",
+    storageBucket: "",
+    messagingSenderId: "39267230178"
+  };
+  firebase.initializeApp(config);
+  
+ var database = firebase.database();
+ var Name = database.ref('firstName');
+ Name.on('value', function(snapshot){
+  var name = snapshot.val();
+  $('.name').html(name);
+ });
+ var Bio = database.ref('Bio');
+ Bio.on('value', function(snapshot){
+  var Bio = snapshot.val();
+  $('.Bio').html(Bio);
+ });
